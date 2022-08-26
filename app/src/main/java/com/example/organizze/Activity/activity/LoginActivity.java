ce013741e331cc4a44b9d992381f821b322a0a52
imiranda <igor.miranda@foursys.com.br>
2022-08-26 16:23:30 -0300

    validacao e autenticacao de usuario(em andamento)

--- app/src/main/java/com/example/organizze/Activity/activity/LoginActivity.java
+++ app/src/main/java/com/example/organizze/Activity/activity/LoginActivity.java
@@ -1,16 +1,100 @@
 package com.example.organizze.Activity.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.organizze.Activity.config.FirebaseHelper;
+import com.example.organizze.Activity.model.User;
 import com.example.organizze.R;
+import com.example.organizze.databinding.ActivityCadastroBinding;
+import com.example.organizze.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private User user;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.butnCadastrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String email = binding.edtEmail.getText().toString();
+                String senha = binding.edtSenha.getText().toString();
+
+                if(!email.isEmpty()){
+                    if(!senha.isEmpty()){
+
+                        user = new User();
+                        user.setEmail(email);
+                        user.setSenha(senha);
+                        validarLogin();
+
+
+                    }else{
+                        Toast.makeText(LoginActivity.this,
+                                "Por favor, informe sua senha", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(LoginActivity.this,
+                            "Por favor, informe seu e-mail.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
+
+    public void validarLogin(){
+
+        autenticacao = FirebaseHelper.getAuth();
+        autenticacao.signInWithEmailAndPassword(
+                user.getEmail(),
+                user.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,
+                            "Login realizado com sucesso", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excecao = "Usuário não está cadastrado";
+                    }catch ( FirebaseAuthInvalidCredentialsException e){
+                        excecao = "E-mail e senha não correspondem a um usuário cadastrado";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar o usuário: "+ e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this,
+                            excecao, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
 }
