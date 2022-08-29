e9ff13a102188ea0db53a8fd24270489ff7f55dd
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-08-29 15:45:51 -0300

    salvando usuario (correcao)

--- app/src/main/java/com/example/organizze/Activity/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/organizze/Activity/activity/PrincipalActivity.java
@@ -42,9 +42,9 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     public void adicionarReceita(View view){
-        startActivity(new Intent(this, DespesasActivity.class));
-    }public void adicionarDespesa(View view){
         startActivity(new Intent(this, ReceitasActivity.class));
+    }public void adicionarDespesa(View view){
+        startActivity(new Intent(this, DespesasActivity.class));
     }
 
     @Override
--- app/src/main/java/com/example/organizze/Activity/model/User.java
+++ app/src/main/java/com/example/organizze/Activity/model/User.java
@@ -1,5 +1,9 @@
 package com.example.organizze.Activity.model;
 
+import com.example.organizze.Activity.config.FirebaseHelper;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
 import java.security.PrivateKey;
 
 public class User {
@@ -7,8 +11,18 @@ public class User {
      private String  nome;
      private String email;
      private String senha;
+     private String idUsuario;
+     private Double receitaTotal = 0.00;
+     private Double despesaTotal = 0.00;
 
     public User() {
+    }
+
+    public void salvar(){
+        DatabaseReference reference = FirebaseHelper.getDatabaseReference();
+        reference.child("usuarios")
+                .child(this.idUsuario)
+                .setValue(this);
     }
 
     public String getNome() {
@@ -27,11 +41,37 @@ public class User {
         this.email = email;
     }
 
+    @Exclude
     public String getSenha() {
         return senha;
     }
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    @Exclude
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Double getReceitaTotal() {
+        return receitaTotal;
+    }
+
+    public void setReceitaTotal(Double receitaTotal) {
+        this.receitaTotal = receitaTotal;
+    }
+
+    public Double getDespesaTotal() {
+        return despesaTotal;
+    }
+
+    public void setDespesaTotal(Double despesaTotal) {
+        this.despesaTotal = despesaTotal;
     }
 }
