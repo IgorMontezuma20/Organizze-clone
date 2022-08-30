e86d6d79a37ad3ace1f9a43a392369a386d9d917
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-08-29 23:08:18 -0300

    metodo para salvar despesa criado

--- app/src/main/java/com/example/organizze/Activity/helper/DateCustom.java
+++ app/src/main/java/com/example/organizze/Activity/helper/DateCustom.java
@@ -12,4 +12,16 @@ public class DateCustom {
         return dataString;
     }
 
+    public static String mesAnoDataEscolhida(String data){
+
+        String retornoData[] = data.split("/");
+        String dia = retornoData[0];
+        String mes = retornoData[1];
+        String ano = retornoData[2];
+
+        String mesAno = mes + ano;
+        return mesAno;
+
+    }
+
 }
---
+++ app/src/main/java/com/example/organizze/Activity/model/Movimentacao.java
@@ -0,0 +1,80 @@
+package com.example.organizze.Activity.model;
+
+import android.widget.Toast;
+
+import com.example.organizze.Activity.config.FirebaseHelper;
+import com.example.organizze.Activity.helper.Base64Custom;
+import com.example.organizze.Activity.helper.DateCustom;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class Movimentacao {
+
+    private String data;
+    private String categoria;
+    private String descricao;
+    private String tipo;
+    private Double valor;
+
+    public Movimentacao() {
+    }
+
+    public void salvar(String dataEscolhida){
+
+        FirebaseAuth auth = FirebaseHelper.getAuth();
+
+        //Recuperando email do user
+        String idUsuario = Base64Custom.codificarBase64(auth.getCurrentUser().getEmail());
+
+        //Recuperando a data da movimentação
+        String mesAno = DateCustom.mesAnoDataEscolhida(dataEscolhida);
+
+        DatabaseReference reference = FirebaseHelper.getDatabaseReference();
+        reference.child("movimentacao")
+                .child(idUsuario)
+                .child(mesAno)
+                .push()
+                .setValue(this);
+
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
