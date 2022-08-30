229986a181592c91f777497f7b31c73bce9c7f53
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-08-29 22:35:43 -0300

    adicionando data atual no campo de data das activities

--- app/src/main/java/com/example/organizze/Activity/activity/DespesasActivity.java
+++ app/src/main/java/com/example/organizze/Activity/activity/DespesasActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.organizze.Activity.helper.DateCustom;
 import com.example.organizze.R;
+import com.example.organizze.databinding.ActivityCadastroBinding;
+import com.example.organizze.databinding.ActivityDespesasBinding;
 
 public class DespesasActivity extends AppCompatActivity {
+
+    private ActivityDespesasBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_despesas);
+        binding = ActivityDespesasBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.edtData.setText(DateCustom.dataAtual());
+
     }
 }
